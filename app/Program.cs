13b4ef66730d0eca22674ca19a5f43cee603e961
Rory Webber <Rory.Webber@arup.com>
2019-12-20 13:59:10 +0000

    updated app to include testing of the arrays class

--- app/Program.cs
+++ app/Program.cs
@@ -7,11 +7,35 @@ namespace app
     {
         static void Main(string[] args)
         {
+            testArrays();
+        }
+
+        static void testRead()
+        {
             int i = BRead.getInt("testing getting int: ");
             string s = BRead.getString("testing getting string: ");
 
             Console.WriteLine($"int: {i}");
             Console.WriteLine($"string: {s}");
+        }
+
+        static void testArrays()
+        {
+            int[] arr = new int[]{1,5,3,2,6,3,2};
+
+            arr = BArrays.BSort.Bubble(arr);
+
+            printArr(arr);
+        }
+
+        static void printArr(int[] arr)
+        {
+            foreach(int i in arr)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine();
         }
     }
 }
